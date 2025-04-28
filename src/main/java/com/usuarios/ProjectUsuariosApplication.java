9c45b6e78bc36b2e79b1b5943ae27daf0743698e
javier-a21 <janduezsan4@educacion.navarra.es>
2025-04-28 10:40:59 +0200

    Correccion de errores en jboss, ahora se permite el despliegue directo en jboss del proyecto

--- src/main/java/com/usuarios/ProjectUsuariosApplication.java
+++ src/main/java/com/usuarios/ProjectUsuariosApplication.java
@@ -2,9 +2,16 @@ package com.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectUsuariosApplication {
+public class ProjectUsuariosApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectUsuariosApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectUsuariosApplication.class, args);
